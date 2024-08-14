e309c7227f3df7d1f16625a78aa600bae14f6aef
ng <0xng@proton.me>
2024-08-15 00:29:10 +0300

    Add load image transform endpoint

--- apps/backend/source/routes/nouns.ts
+++ apps/backend/source/routes/nouns.ts
@@ -4,10 +4,12 @@ import v1DaoController from '../controllers/v1/dao'
 import v1ImageController from '../controllers/v1/image'
 
 import daoController from '../controllers/dao'
+import imageController from '../controllers/image'
 
 const router = express.Router()
 
 router.get('/dao/:chain/:address', daoController.getData)
+router.get('/image/from-url', imageController.getData)
 
 router.get('/dao/:slug', v1DaoController.getData)
 router.get('/image/:address/:id', v1ImageController.getData)
