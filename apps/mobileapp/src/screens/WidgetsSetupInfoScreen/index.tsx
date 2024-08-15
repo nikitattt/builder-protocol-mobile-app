9a023bcaad0e8229e844fa167294e68ad1f3e22d
ng <0xng@proton.me>
2024-08-15 19:02:59 +0300

    Added modal to direct to widget install instructions

--- apps/mobileapp/src/screens/WidgetsSetupInfoScreen/index.tsx
+++ apps/mobileapp/src/screens/WidgetsSetupInfoScreen/index.tsx
@@ -3,11 +3,26 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { RootStackScreenProps } from '../../navigation/types'
 import BackButton from '../../components/BackButton'
 import WidgetInstallInstructions from '../../components/WidgetsInstallInstructions'
+import { useIntroStore } from '../../store/intro'
+import { useEffect } from 'react'
 
 const WidgetsSetupInfoScreen = ({
   route,
   navigation
 }: RootStackScreenProps<'WidgetsSetupInfo'>) => {
+  const widgetsInstructionsModalSeen = useIntroStore(
+    state => state.widgetsInstructionsModalSeen
+  )
+  const setWidgetsInstructionsModalSeen = useIntroStore(
+    state => state.setWidgetsInstructionsModalSeen
+  )
+
+  useEffect(() => {
+    if (!widgetsInstructionsModalSeen) {
+      setWidgetsInstructionsModalSeen(true)
+    }
+  }, [widgetsInstructionsModalSeen, setWidgetsInstructionsModalSeen])
+
   return (
     <View className="flex-1 bg-white">
       <SafeAreaView className="flex-1">
