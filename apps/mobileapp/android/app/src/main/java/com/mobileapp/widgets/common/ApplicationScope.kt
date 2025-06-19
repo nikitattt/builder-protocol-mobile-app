a1b8ce9edcff2f463ce2311ae89368c553ee1394
NG <0xng@proton.me>
2025-06-19 23:21:19 +0300

    Remove ApplicationScope.kt as it is no longer needed, streamlining the codebase.

--- apps/mobileapp/android/app/src/main/java/com/mobileapp/widgets/common/ApplicationScope.kt
+++
@@ -1,9 +0,0 @@
-package com.mobileapp.widgets.common
-
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-
-object ApplicationScope {
-    val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
-} 
